875903632ebeb8b77b1ff1bffbb9989fb7e8282a
Jay-Man-Web <pfunzojohn06@gmail.com>
2026-03-27 03:27:43 +0200

    Add files via upload

--- scripts/projects.js
+++ scripts/projects.js
@@ -5,6 +5,14 @@ const projects = [{
   tools: ['HTML', 'CSS', 'JavaScript'],
   status: 'In Progress',
   link: 'projects/TheComicDrop.html'
+}, 
+{
+  name: "Site Sprint",
+  thumbnail: "images/thumbnails/the-comic-drop.png",
+  description: "The Comic Drop – Personal Project </br> A simple, user-friendly website for comic news and discussions. Designed to be fast, clear, and easy to navigate, it showcases my front-end skills while creating a space for comic fans to explore and enjoy content.",
+  tools: ['HTML', 'CSS', 'JavaScript'],
+  status: 'In Progress',
+  link: 'projects/TheComicDrop.html'
 }];
 
 let projectsHTML = '';
@@ -18,22 +26,24 @@ projects.forEach((project) => {
 
   projectsHTML += `
   <h3>${project.name}</h3>
-  <div class="project-container">
-    <div class="thumbnail-wrap">
-      <img class="thumbnail" src="${project.thumbnail}" alt="${project.name}">
-    </div>
-    <div class="description-wrap">
-      <p>
-        ${project.description}
-      </p>
-      <div class="tools-wrap">
-        ${toolsHTML}
-        <span class="tools status">
-          ${project.status}
-        </span>
+  <a class="project-link" href="${project.link}">
+    <div class="project-container">
+      <div class="thumbnail-wrap">
+        <img class="thumbnail" src="${project.thumbnail}" alt="${project.name}">
+      </div>
+      <div class="description-wrap">
+        <p>
+          ${project.description}
+        </p>
+        <div class="tools-wrap">
+          ${toolsHTML}
+          <span class="tools status">
+            ${project.status}
+          </span>
+        </div>
       </div>
     </div>
-  </div>
+  </a>
   `;
 });
 
